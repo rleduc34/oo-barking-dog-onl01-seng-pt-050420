b951e326ddd4566455b0b6b98d8b4036ffed705b
Robert Leduc <rleduc34@gmail.com>
2020-05-22 00:44:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog
+  def name=(dog_name)
+    @dog_barks = dog_name
+  end
+
+  def name
+    @dog_barks
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+
+fido = Dog.new
+fido.name = "Fido"

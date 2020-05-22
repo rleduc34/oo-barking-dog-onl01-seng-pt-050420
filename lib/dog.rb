fd4ca4c3f4654c4fbc46303436ef4b50d8a035df
Robert Leduc <rleduc34@gmail.com>
2020-05-22 00:42:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,16 @@
 class Dog
   def name=(dog_name)
-    dog_barks = dog_name
+    @dog_barks = dog_name
   end
 
-  def name
-    dog_barks
-
+  def name 
+    @dog_barks
   end
+
+  def bark
+    puts "woof!"
+  end
+end
 
 fido = Dog.new
 fido.name = "Fido"
